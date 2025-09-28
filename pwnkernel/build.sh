31937e88473dc1c46a82db196d5b44b3a643e9e2
Tin Vuong <vuonghieunghia19122003@gmail.com>
2025-09-28 16:37:49 -0400

    Changed system() to execve() in get_shell() of the exploit/solve script

--- pwnkernel/build.sh
+++ pwnkernel/build.sh
@@ -16,52 +16,52 @@ export BUSYBOX_VERSION=1.36.0
 #
 
 if [ ! -d "linux-$KERNEL_VERSION" ]; then
-	echo "[+] Downloading kernel..."
-	wget -q -c https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/linux-$KERNEL_VERSION.tar.gz
-	[ -e linux-$KERNEL_VERSION ] || tar xzf linux-$KERNEL_VERSION.tar.gz
+    echo "[+] Downloading kernel..."
+    wget -q -c https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/linux-$KERNEL_VERSION.tar.gz
+    [ -e linux-$KERNEL_VERSION ] || tar xzf linux-$KERNEL_VERSION.tar.gz
 else
-	echo "[+] Found kernel source dir in current dir. Using it instead of downloading a new one..."
+    echo "[+] Found kernel source dir in current dir. Using it instead of downloading a new one..."
 fi
 
-read -p  "Rebuild kernel? [Y/N] " rebuild
+read -p "Rebuild kernel? [Y/N] " rebuild
 if [[ "$rebuild" =~ [yY] ]]; then
 
-	echo "[+] Building kernel..."
-	make -C linux-$KERNEL_VERSION defconfig
-	echo "CONFIG_NET_9P=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_NET_9P_DEBUG=n" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_9P_FS=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_9P_FS_POSIX_ACL=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_9P_FS_SECURITY=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_NET_9P_VIRTIO=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_PCI=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_BLK=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_BLK_SCSI=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_NET=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_CONSOLE=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_HW_RANDOM_VIRTIO=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DRM_VIRTIO_GPU=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_PCI_LEGACY=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_BALLOON=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_VIRTIO_INPUT=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_CRYPTO_DEV_VIRTIO=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_BALLOON_COMPACTION=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_PCI=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_PCI_HOST_GENERIC=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_GDB_SCRIPTS=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DEBUG_INFO=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DEBUG_INFO_REDUCED=n" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DEBUG_INFO_SPLIT=n" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DEBUG_FS=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DEBUG_INFO_DWARF4=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_DEBUG_INFO_BTF=y" >> linux-$KERNEL_VERSION/.config
-	echo "CONFIG_FRAME_POINTER=y" >> linux-$KERNEL_VERSION/.config
+    echo "[+] Building kernel..."
+    make -C linux-$KERNEL_VERSION defconfig
+    echo "CONFIG_NET_9P=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_NET_9P_DEBUG=n" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_9P_FS=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_9P_FS_POSIX_ACL=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_9P_FS_SECURITY=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_NET_9P_VIRTIO=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_PCI=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_BLK=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_BLK_SCSI=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_NET=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_CONSOLE=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_HW_RANDOM_VIRTIO=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DRM_VIRTIO_GPU=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_PCI_LEGACY=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_BALLOON=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_VIRTIO_INPUT=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_CRYPTO_DEV_VIRTIO=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_BALLOON_COMPACTION=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_PCI=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_PCI_HOST_GENERIC=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_GDB_SCRIPTS=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DEBUG_INFO=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DEBUG_INFO_REDUCED=n" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DEBUG_INFO_SPLIT=n" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DEBUG_FS=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DEBUG_INFO_DWARF4=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_DEBUG_INFO_BTF=y" >>linux-$KERNEL_VERSION/.config
+    echo "CONFIG_FRAME_POINTER=y" >>linux-$KERNEL_VERSION/.config
 
-	sed -i 'N;s/WARN("missing symbol table");\n\t\treturn -1;/\n\t\treturn 0;\n\t\t\/\/ A missing symbol table is actually possible if its an empty .o file.  This can happen for thunk_64.o./g' linux-$KERNEL_VERSION/tools/objtool/elf.c
+    sed -i 'N;s/WARN("missing symbol table");\n\t\treturn -1;/\n\t\treturn 0;\n\t\t\/\/ A missing symbol table is actually possible if its an empty .o file.  This can happen for thunk_64.o./g' linux-$KERNEL_VERSION/tools/objtool/elf.c
 
-	sed -i 's/unsigned long __force_order/\/\/ unsigned long __force_order/g' linux-$KERNEL_VERSION/arch/x86/boot/compressed/pgtable_64.c
+    sed -i 's/unsigned long __force_order/\/\/ unsigned long __force_order/g' linux-$KERNEL_VERSION/arch/x86/boot/compressed/pgtable_64.c
 
-	make -C linux-$KERNEL_VERSION -j16 bzImage
+    make -C linux-$KERNEL_VERSION -j16 bzImage
 fi
 
 #
@@ -69,17 +69,17 @@ fi
 #
 
 if [ ! -d busybox-$BUSYBOX_VERSION ]; then
-	echo "[+] Downloading busybox..."
-	wget -q -c https://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
-	[ -e busybox-$BUSYBOX_VERSION ] || tar xjf busybox-$BUSYBOX_VERSION.tar.bz2
+    echo "[+] Downloading busybox..."
+    wget -q -c https://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
+    [ -e busybox-$BUSYBOX_VERSION ] || tar xjf busybox-$BUSYBOX_VERSION.tar.bz2
 
-	echo "[+] Building busybox..."
-	make -C busybox-$BUSYBOX_VERSION defconfig
-	sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/g' busybox-$BUSYBOX_VERSION/.config
-	make -C busybox-$BUSYBOX_VERSION -j16
-	make -C busybox-$BUSYBOX_VERSION install
+    echo "[+] Building busybox..."
+    make -C busybox-$BUSYBOX_VERSION defconfig
+    sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/g' busybox-$BUSYBOX_VERSION/.config
+    make -C busybox-$BUSYBOX_VERSION -j16
+    make -C busybox-$BUSYBOX_VERSION install
 else
-	echo "[+] Found busybox-$BUSYBOX_VERSION directory. Using it instead of rebuilding..." 
+    echo "[+] Found busybox-$BUSYBOX_VERSION directory. Using it instead of rebuilding..."
 fi
 
 #
@@ -92,6 +92,7 @@ mkdir -p bin sbin etc proc sys usr/bin usr/sbin root home/ctf home/modules
 cd ..
 cp -a busybox-$BUSYBOX_VERSION/_install/* fs
 cp ./qemu_init fs/init
+cp ./flag fs/
 chmod +x fs/init
 
 #
@@ -103,9 +104,8 @@ fs_copy_dir=$PWD/fs/home/modules
 linux_dir=$PWD/linux-$KERNEL_VERSION
 cd src
 for dir in $(find $PWD/ -type d); do
-	make -C $dir || continue
-	cp $dir/*.ko $fs_copy_dir || continue
-# needs these modules to be present in the linux-X-X dir as well in order for gdb kernel scripts to load their debug info.
-	cp $dir/*.ko $linux_dir || continue 
+    make -C $dir || continue
+    cp $dir/*.ko $fs_copy_dir || continue
+    # needs these modules to be present in the linux-X-X dir as well in order for gdb kernel scripts to load their debug info.
+    cp $dir/*.ko $linux_dir || continue
 done
-
